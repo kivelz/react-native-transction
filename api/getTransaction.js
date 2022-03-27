3ed5cf530a3da8acf9b8a18f591c2b3a8a3fb1b4
Kelvin Tay <kelvintay@Kelvins-MacBook-Pro.local>
2022-03-27 13:39:51 +0800

    change to axios to get mock api

--- api/getTransaction.js
+++ api/getTransaction.js
@@ -4,24 +4,47 @@ import { NavigationContainer, useNavigationContainerRef } from '@react-navigatio
 import Notification from '../app/view/screen/NotificationScreen'
 
 import customerData from  './data.json'
+import axios from 'axios';
 
 
 const TransactionData = props => {
-  
+  const [isLoading, setLoading] = useState(false);
+  const [transaction, setTransaction] = useState([]);
+  const [getError, setError] = useState(null);
+
+  const getData = () => { 
+    axios
+    .get('https://623ff07f0adaf66ad748df81.mockapi.io/api/v1/transactions')
+    .then(function (response) {
+      // handle success
+      console.log('responses', response.data)
+      setTransaction(response.data)
+    })
+    .catch(function (error) {
+      // handle error
+      alert(error.message);
+    })
+};
+  useEffect(() => {
+      setLoading(true);
+      getData();
+  }, []);
+
   const navigateToDetailScreen = (id) => {
     props.navigation.navigate("DetailPage", {id: id})
   }
-
+  console.log('alltransaction', transaction)
   return(
     <View>
         <View>
             <View style={{paddingVertical: 20, alignItems:'center'}}> 
-            {customerData.transactions.map((item, index) => (
+            {transaction.map((item, index) => (
               <TouchableOpacity style={{marginVertical: 5}} onPress={() => navigateToDetailScreen(item.id)}>
                        <Text style={{color:'#0000EE', fontSize:16}}>Transaction {item.id}</Text>
               </TouchableOpacity>       
             ))}
-            </View>         
+            </View>       
+          
     </View>
       </View>
   );
